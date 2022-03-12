5104c7975c8294098ff69afcd4174886f63d1744
Sebastian Rollen <rollenseb@gmail.com>
2022-03-11 20:06:42 -0500

    more mutt settings

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 sudo rm -rf ~/.vimrc > /dev/null 2>&1
 sudo rm -rf ~/.gitconfig > /dev/null 2>&1
 sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
+sudo rm -rf ~/.mutt > /dev/null 2>&1
 
 ln -sf ~/src/dotfiles/vimrc ~/.vimrc
 ln -sf ~/src/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/src/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/src/dotfiles/mutt ~/.mutt
